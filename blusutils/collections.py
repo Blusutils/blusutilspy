157f3561dc58b90946dab9b5213d6817d9fcbd7e
Егор Брон <bataikinegor@yandex.ru>
2022-01-15 18:53:08 +0300

    Update collections.py

--- blusutils/collections.py
+++ blusutils/collections.py
@@ -16,78 +16,74 @@ def merge(collection: typing.Iterable, *args: typing.Iterable) -> list:
     return collection
 
 class DinfQueue():
-    def __init__(self, *args, collection: list = []):
-        self.collection = []
-        if args and not collection:
-            self.collection = list(args)
-        elif collection and not args:
-            self.collection = collection
-        elif not collection and not args:
-            pass
-        else:
-            raise AttributeError('args and collection provided')
-        self.index = 0
-    def __repr__(self):
-        return f'DinfQueue({str(self.collection)})'
-    def __len__(self):
-        return len(self.collection)
-    def __reversed__(self):
-        return DinfQueue(collection = self.collection[::-1])
-    def __contains__(self, item):
-        return item in self.collection
-    def __getitem__(self, key):
-        return self.collection[key] if isinstance(key, int) else None
-    def __setitem__(self, key, value):
-        if isinstance(key, int):
-            self.collection[key] = value
-        else:
-            raise TypeError(f'key must be {int}, not {type(key)}')
-    def __delitem__(self, key):
-        del self.collection[key]
-    def __iter__(self):
-        return iter(self.collection)
-    def __next__(self):
-        try:
-            self.index += 1
-            return self.collection[self.index-1]
-        except IndexError:
-            raise StopIteration
-    def __aiter__(self):
-        return iter(self.collection)
-    def __anext__(self):
-        try:
-            self.index += 1
-            return self.collection[self.index-1]
-        except IndexError:
-            raise StopAsyncIteration
-    def append(self, obj: object):
-        self.collection.append(obj)
-    def shuffle(self):
-        self.collection = self.temp_shuffle()
-        return self
-    def temp_shuffle(self):
-        result = []
-        copied = self.collection.copy()
-        while copied:
-            selected = random.chioce(copied)
-            copied.remove(selected)
-            result.append(selected)
-        return result
-    # def next(self):
-    # 	try:
-    # 		self.index += 1
-    # 		return self.collection[self.index-1]
-    # 	except IndexError:
-    # 		return None
-    def reset(self):
-        self.index = 0
-    def clear(self):
-        self.collection = []
-        self.index = 0
-    def upcoming(self):
-        return self.collection[self.index+1:]
-    def now(self):
-        return self.collection[self.index]
+	collection = []
+	def __init__(self, *args, collection: list = []):
+		self.collection = [] if not self.collection else self.collection
+		if args and not collection:
+			self.collection = list(args)
+		elif collection and not args:
+			self.collection = collection
+		elif not collection and not args:
+			pass
+		else:
+			raise AttributeError('args and collection provided')
+		self.index = 0
+	def __repr__(self):
+		return f'<DinfQueue({str(self.collection)}) object at {("0x")+(hex(id(self))[2:].capitalize())}>'
+	def __len__(self):
+		return len(self.collection)
+	def __reversed__(self):
+		return DinfQueue(collection = self.collection[::-1])
+	def __contains__(self, item):
+		return item in self.collection
+	def __getitem__(self, key):
+		return self.collection[key] if isinstance(key, int) else None
+	def __setitem__(self, key, value):
+		if isinstance(key, int):
+			self.collection[key] = value
+		else:
+			raise TypeError(f'key must be {int}, not {type(key)}')
+	def __delitem__(self, key):
+		del self.collection[key]
+	def __iter__(self):
+		return iter(self.collection)
+	def __next__(self):
+		try:
+			self.index += 1
+			return self.collection[self.index-1]
+		except IndexError:
+			self.index -= 1
+			raise StopIteration
+	def __aiter__(self):
+		return iter(self.collection)
+	def __anext__(self):
+		try:
+			self.index += 1
+			return self.collection[self.index-1]
+		except IndexError:
+			raise StopAsyncIteration
+	def append(self, obj: object):
+		self.collection.append(obj)
+	def shuffle(self):
+		self.collection = self.temp_shuffle()
+		return self
+	def temp_shuffle(self):
+		return random.shuffle(self.collection.copy())
+	# def next(self):
+	# 	try:
+	# 		self.index += 1
+	# 		return self.collection[self.index-1]
+	# 	except IndexError:
+	# 		return None
+	def reset(self):
+		self.index = 0
+	def clear(self):
+		self.collection = []
+		self.index = 0
+	def upcoming(self):
+		return self.collection[self.index+1:]
+	def now(self):
+		return self.collection[self.index]
 
 class Line:
     # i think this is an collection
