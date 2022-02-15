6d37c2aa4144035995ebdc6636359eba518300ac
EgorBron <bataikinegor@yandex.ru>
2022-02-15 21:52:02 +0300

    Update __init__.py

--- blusutils/__init__.py
+++ blusutils/__init__.py
@@ -4,6 +4,7 @@ from .syntax_constructions import *
 from . import crypto
 from . import other
 from . import textutils
+from . import forismatic
 # from browser import Browser
 
 __version__ = '0.0.1'
