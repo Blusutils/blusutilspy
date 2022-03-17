3701f102397b9cf6510d43af6244210a3d4e3861
Егор Брон <bataikinegor@yandex.ru>
2022-03-17 15:20:04 +0300

    Release prepared

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blusutils",
+    version="0.0.1.1",
+    author="Blusutils",
+    author_email="contact@blusutils.net",
+    description="Library with random functionality.",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Blusutils/blusutilspy",
+    project_urls={
+        "Bug Tracker": "https://github.com/Blusutils/blusutilspy/issues",
+        'Blusutils GitHub': 'https://github.com/Blusutils/',
+        'Blusutils Website': 'https://blusutils.net'
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    packages=setuptools.find_packages(where='.'),
+    python_requires=">=3.9",
+    install_requires = ['selenium', 'rapidjson'],#(reqs:=open('requirements.txt')).readlines(),
+    zip_save = False
+)
+#reqs.close()
