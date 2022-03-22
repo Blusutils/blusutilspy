f7b151abe406df3f1deb5b7f9a7addf56237b7aa
Егор Брон <bataikinegor@yandex.ru>
2022-03-22 09:58:58 +0300

    Upd: JLJS won't wixed

--- blusutils/collections.py
+++ blusutils/collections.py
@@ -208,24 +208,24 @@ class Line:
 
 
 # Bug: getattr goes to recursion
-class JLJS():
-    """JSON object like "objects" in JS"""
-    def __init__(self, based_dict: dict = {}, **kwargs) -> None:
-        self._JSON = based_dict
-        if kwargs:
-            for kw, kv in kwargs:
-                self._JSON[kw] = kv
-    def __getattr__(self, attr):
-        return self._JSON.get(attr, None)
-    def __getitem__(self, attr):
-        return self._JSON.get(attr, None)
-    def __setattr__(self, name: str, value: typing.Any) -> None:
-        self._JSON.__setattr__(name, value)
-    def __setitem__(self, name: str, value: typing.Any) -> None:
-        self.__setattr__(name, value)
-    def __repr__(self):
-        return f"<JLJS keys={self._JSON.keys()}>"
-    def __str__(self):
-        return rjson.dumps(self._JSON, indent=4, ensure_ascii=False)
-    def stringify(self):
-        return self.__str__()
+# class JLJS():
+#     """JSON object like "objects" in JS"""
+#     def __init__(self, based_dict: dict = {}, **kwargs) -> None:
+#         self._JSON = based_dict
+#         if kwargs:
+#             for kw, kv in kwargs:
+#                 self._JSON[kw] = kv
+#     def __getattr__(self, attr):
+#         return self._JSON.get(attr, None)
+#     def __getitem__(self, attr):
+#         return self._JSON.get(attr, None)
+#     def __setattr__(self, name: str, value: typing.Any) -> None:
+#         self._JSON.__setattr__(name, value)
+#     def __setitem__(self, name: str, value: typing.Any) -> None:
+#         self.__setattr__(name, value)
+#     def __repr__(self):
+#         return f"<JLJS keys={self._JSON.keys()}>"
+#     def __str__(self):
+#         return rjson.dumps(self._JSON, indent=4, ensure_ascii=False)
+#     def stringify(self):
+#         return self.__str__()
